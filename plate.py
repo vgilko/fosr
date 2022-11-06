03b750bdd824d50f844d0c8c21080c1bca81ea11
Viktor Gilko <vgilkojob@gmail.com>
2022-11-07 02:18:51 +0300

    временное решение 2

--- plate.py
+++ plate.py
@@ -33,12 +33,12 @@ class Plate:
                  hole: Hole,
                  dx=0.5,
                  dy=0.5,
-                 thermal_conductivity=300,
-                 material_density=10,
-                 heat_capacity=900,
-                 start_temperature=20.0,
-                 left_border_temperature=30,
-                 right_border_temperature=30):
+                 start_temperature=1273,
+                 left_border_temperature=1900,
+                 right_border_temperature=1900,
+                 thermal_conductivity=60,
+                 material_density=3070,
+                 heat_capacity=1260):
         self.thermal_conductivity = thermal_conductivity
         self.material_density = material_density
         self.heat_capacity = heat_capacity
@@ -47,21 +47,19 @@ class Plate:
         self.thermal_diffusivity = self.thermal_conductivity / (material_density * heat_capacity)
         self.dx = dx
         self.dy = dy
+        self.hole = hole
+
+        self.hole_y_idx = -1
 
         self.__build_axis(dx, dy, height, width)
 
         self.__build_matrix()
 
-        if hole is not None:
-            self.__make_hole(hole)
-
         self.__set_temperature(start_temperature)
 
-        print(self.matrix)
-
     def __build_axis(self, dx, dy, height, width):
-        self.y = np.arange(0, height + dy, dy)
-        self.x = np.arange(0, width + dx, dx)
+        self.y = np.arange(0, height + dy / 1000, dy)
+        self.x = np.arange(0, width + dx / 1000, dx)
 
     def __build_matrix(self):
         shape_y = self.y.shape[0]
@@ -71,18 +69,19 @@ class Plate:
         temp_array = np.zeros(amount, float)
         self.matrix = temp_array.reshape(shape_y, shape_x)
 
-    def __make_hole(self, hole: Hole):
-        hole_start_y = hole.get_start_y()
-        hole_start_x = hole.get_start_x()
+    def make_hole(self):
+        if self.hole is not None:
+            hole_start_y = self.hole.get_start_y()
+            hole_start_x = self.hole.get_start_x()
 
-        hole_end_x = hole.get_end_x()
-        hole_end_y = hole.get_end_y()
+            hole_end_x = self.hole.get_end_x()
+            hole_end_y = self.hole.get_end_y()
 
-        for idx_y, value_y in enumerate(self.y):
-            is_hole = hole_start_y <= value_y <= hole_end_y
-            for idx_x, value_x in enumerate(self.x):
-                if hole_start_x <= value_x <= hole_end_x and is_hole:
-                    self.matrix[idx_x, idx_y] = np.NaN
+            for idx_y, value_y in enumerate(self.y):
+                is_hole = hole_start_y <= value_y <= hole_end_y
+                for idx_x, value_x in enumerate(self.x):
+                    if hole_start_x <= value_x <= hole_end_x and is_hole:
+                        self.matrix[idx_x, idx_y] = np.NaN
 
     def __set_temperature(self, temperature):
         self.matrix[~np.isnan(self.matrix)] = temperature
--- scheme_solver.py
+++ scheme_solver.py
@@ -1,13 +1,12 @@
 from math import sqrt
 
+import matplotlib.pyplot as plt
+
 from plate import *
 
 
-def solve(plate: Plate, end_time=100):
+def solve(plate: Plate, end_time=1000):
     tau = end_time / 100
-    time = 0
-
-    print(plate.matrix.shape)
 
     axis_y_shape = plate.matrix.shape[0]
     axis_x_shape = plate.matrix.shape[1]
@@ -15,48 +14,62 @@ def solve(plate: Plate, end_time=100):
     alfa = np.zeros(axis_x_shape)
     beta = np.zeros(axis_x_shape)
 
+    time = 0
+
+    sqrt_dy = sqrt(plate.dy)
+    ci = plate.thermal_conductivity / sqrt(plate.dx)
+    atau_coeff = 2.0 * plate.thermal_diffusivity * tau
+    thermal_conductivity_sqrt = plate.thermal_conductivity / sqrt_dy
+
     while time < end_time:
         time += tau
 
-        ai = plate.thermal_conductivity / sqrt(plate.dx)
-        bi = 2.0 * plate.thermal_conductivity / sqrt(plate.dx) + plate.material_density * plate.heat_capacity / tau
-        for j in range(axis_y_shape - 1):
+        # ai = plate.thermal_conductivity / sqrt(plate.dx)
+        bi = 2.0 * ci + plate.material_density * plate.heat_capacity / tau
+        for j in range(axis_y_shape):
             alfa[1] = 0
             beta[1] = plate.left_border_temperature
 
             for i in range(1, axis_x_shape):
-                ci = plate.thermal_conductivity / sqrt(plate.dx)
-                fi = -plate.material_density * plate.heat_capacity * plate.matrix[i][j] / tau
-                alfa[i] = ai / (bi - ci * alfa[i - 1])
+                fi = -plate.material_density * plate.heat_capacity * plate.matrix[j][i] / tau
+                alfa[i] = ci / (bi - ci * alfa[i - 1])
                 beta[i] = (ci * beta[i - 1] - fi) / (bi - ci * alfa[i - 1])
 
-            plate.matrix[axis_x_shape, j] = plate.right_border_temperature
+            plate.matrix[j][axis_x_shape - 1] = plate.right_border_temperature
 
-            for i in range(axis_x_shape - 1, 1, -1):
-                plate.matrix[i][j] = alfa[i] * plate.matrix[i + 1][j] + beta[i]
+            for i in range(axis_x_shape - 2, 1, -1):
+                plate.matrix[j][i] = alfa[i] * plate.matrix[j][i + 1] + beta[i]
 
-        for i in range(1, axis_x_shape):
-            alfa[1] = 2.0 * plate.thermal_diffusivity * tau / (2.0 * plate.thermal_diffusivity * tau + sqrt(plate.dy))
-            beta[1] = sqrt(plate.dy) * plate.matrix[i][1] / (2.0 * plate.thermal_diffusivity * tau + sqrt(plate.dy))
+        for i in range(1, axis_x_shape - 1):
+            alfa[1] = atau_coeff / (atau_coeff + sqrt_dy)
+            beta[1] = sqrt_dy * plate.matrix[1][i] / (atau_coeff + sqrt_dy)
 
-            for j in range(1, axis_y_shape - 1):
-                ai = plate.thermal_conductivity / sqrt(plate.dy)
-                bi = 2.0 * plate.thermal_conductivity / sqrt(
-                    plate.dy) + plate.material_density * plate.heat_capacity / tau
-                ci = plate.thermal_conductivity / sqrt(plate.dy)
-                fi = -plate.material_density * plate.heat_capacity * plate.matrix[i][j] / tau
+            for j in range(1, axis_y_shape):
+                ai = thermal_conductivity_sqrt
+                bi = 2.0 * thermal_conductivity_sqrt + plate.material_density * plate.heat_capacity / tau
+                ci = thermal_conductivity_sqrt
+                fi = -plate.material_density * plate.heat_capacity * plate.matrix[j][i] / tau
 
                 alfa[j] = ai / (bi - ci * alfa[j - 1])
                 beta[j] = (ci * beta[j - 1] - fi) / (bi - ci * alfa[j - 1])
 
-            plate.matrix[i][axis_y_shape] = (2.0 * plate.thermal_diffusivity * tau * beta[axis_y_shape - 1] + sqrt(
-                plate.dy) * plate.matrix[i][axis_y_shape]) / (2.0 * plate.thermal_diffusivity * tau *
-                                                              (1.0 - alfa[axis_y_shape - 1]) + sqrt(plate.dy))
+            plate.matrix[axis_y_shape - 1][i] = (atau_coeff * beta[axis_y_shape - 2] +
+                                                 sqrt_dy * plate.matrix[axis_y_shape - 1][i]) / \
+                                                (atau_coeff * (1.0 - alfa[axis_y_shape - 2]) + sqrt_dy)
 
-            for j in range(axis_y_shape - 1, 0, -1):
-                plate.matrix[i][j] = alfa[j] * plate.matrix[i][j + 1] + beta[j]
+            for j in range(axis_y_shape - 2, 0, -1):
+                plate.matrix[j][i] = alfa[j] * plate.matrix[j + 1][i] + beta[j]
 
 
-testing_plate = Plate(5, 10, Hole(Point(1, 1), 1, 1), 1, 1)
+testing_plate = Plate(10, 10, Hole(Point(4, 4), 4, 4), 0.2, 0.2)
 
 solve(testing_plate)
+
+testing_plate.make_hole()
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+X, Y = np.meshgrid(testing_plate.x, testing_plate.y)
+ax.plot_surface(X, Y, testing_plate.matrix, rstride=1, cstride=1, cmap='viridis')
+
+plt.show()
